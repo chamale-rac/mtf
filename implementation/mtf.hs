8a62e51ced1ec26d195c41c4112ac8821a23bb32
Samuel Chamalé <samuel.chamale23@gmail.com>
2024-05-06 20:35:27 -0600

    feat: complete mtf
    - mtf configurations and and solicitudes

--- implementation/mtf.hs
+++ implementation/mtf.hs
@@ -26,14 +26,26 @@ processRequests xs requests = foldl process (xs, []) requests
       let (newList, cost) = access req lst
        in (newList, acc ++ [(newList, req, cost)])
 
+-- Function to process multiple initial list and request tuples
+processMultipleRequests :: [(MTFList, [Int])] -> IO ()
+processMultipleRequests = mapM_ processTuple
+  where
+    processTuple (initialList, requests) = do
+      let (finalList, results) = processRequests initialList requests
+      mapM_ printResult results
+      let totalCost = sum $ map (\(_, _, cost) -> cost) results
+      putStrLn $ "Total Cost of Accesses: " ++ show totalCost
+    printResult (lst, req, cost) = putStrLn $ "List: " ++ show lst ++ ", Request: " ++ show req ++ ", Cost: " ++ show cost
+    -- Print a divider between each tuple
+    printDivider = putStrLn "----------------------------------------"
+
 -- Main function to run the example
 main :: IO ()
 main = do
-  let initialList = [0, 1, 2, 3, 4]
-  let requests = [0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4]
-  let (finalList, results) = processRequests initialList requests
-  mapM_ printResult results
-  let totalCost = sum $ map (\(_, _, cost) -> cost) results
-  putStrLn $ "Total Cost of Accesses: " ++ show totalCost
-  where
-    printResult (lst, req, cost) = putStrLn $ "List: " ++ show lst ++ ", Request: " ++ show req ++ ", Cost: " ++ show cost
+  let tuples = [([0, 1, 2, 3, 4], [40, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4, 0, 1, 2, 3, 4]), 
+                ([0, 1, 2, 3, 4], [4, 3, 2, 1, 0, 1, 2, 3, 4, 3, 2, 1, 0, 1, 2, 3, 4]),
+                ([0, 1, 2, 3, 4], [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]), -- Minimum cost
+                ([0, 1, 2, 3, 4], [4, 3, 2, 1, 0, 4, 3, 2, 1, 0, 4, 3, 2, 1, 0, 4, 3, 2, 1, 0]), -- Maximum cost
+                ([0, 1, 2, 3, 4], [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]),
+                ([0, 1, 2, 3, 4], [3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3])] 
+  processMultipleRequests tuples
